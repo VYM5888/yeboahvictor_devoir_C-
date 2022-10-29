4745244e8ba9f09d79e8fedc64d16d13efd1cf55
VYM5888 <yeboahvictor5888@outlook.com>
2022-10-29 18:38:13 +0100

    complete work

---
+++ inserer_entier/inserer_entier/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    static void Main()
+    {
+        int i;
+        int[] a = new int[30];   
+        Console.Write("saisir le nombre de values: ");
+          
+        int n = Convert.ToInt16(Console.ReadLine());
+        
+        for (i = 1; i <= n; i++)
+        {
+            Console.Write("Enterer No " + i + ":");
+              
+            a[i] = Convert.ToInt16(Console.ReadLine());
+        }
+         
+        for (i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n - 1; j++)
+            {
+                if (a[j] > a[j + 1])
+                {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+          
+        Console.Write("ordre : ");
+        for (i = 1; i <= n; i++)
+        {
+            Console.Write(a[i] + " ");
+        }
+        
+        Console.ReadKey();
+    }
+}
---
+++ tp_premier nombre/tp_premier nombre/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+public class PrimeNumberExample
+{
+    public static void Main(string[] args)
+    {
+        int n, i, m = 0, flag = 0;
+        Console.Write("saisit une nombre: ");
+        n = int.Parse(Console.ReadLine());
+        m = n / 2;
+        for (i = 2; i <= m; i++)
+        {
+            if (n % i == 0)
+            {
+                Console.Write("le nombre n'est pas premier .");
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0)
+            Console.Write("le nombre est premier.");
+    }
+}
